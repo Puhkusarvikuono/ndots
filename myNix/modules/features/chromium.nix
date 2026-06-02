f0ca55273cd0d348a985085d05f9013921386b30
puhkusarvikuono <puhkusarvikuono@proton.me>
2026-06-02 20:40:25 +0300

    chromium extensions

--- myNix/modules/features/chromium.nix
+++ myNix/modules/features/chromium.nix
@@ -2,6 +2,10 @@
   flake.nixosModules.chromium = {pkgs, lib, ...}: {
     programs.chromium = {
         enable = true;
+        extensions = [
+            "ghmbeldphafepmbegfdlkpapadhbakde" # Proton Pass
+            "eimadpbcbfnmbkopoojfekhnkhdbieeh;https://clients2.google.com/service/update2/crx" # dark reader
+          ];
     };
 
     environment.systemPackages = [
