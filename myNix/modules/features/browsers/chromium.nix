3243934c4899771a8a44929d823481fc1d20961f
Puhkusarvikuono <puhkusarvikuono@proton.me>
2026-08-29 10:19:21 +0300

    chromium settings

--- myNix/modules/features/browsers/chromium.nix
+++ myNix/modules/features/browsers/chromium.nix
@@ -6,6 +6,10 @@
       programs.chromium = {
         enable = true;
         extraOpts = {
+            "AudioSandboxEnabled" = false;
+            "AutofillAddressEnabled" = false;
+            "AutofillCreditCardEnabled" = false;
+            "BlockThirdPartyCookies" = true;
             "BraveAIChatEnabled" = false; # Disable Brave AI Chat
             "BraveNewsDisabled" = true; # Disable Brave News
             "BraveRewardsDisabled" = true; # Disable Brave Rewards
@@ -15,6 +19,10 @@
             "BraveWalletDisabled" = true; # Disable Brave Wallet
             "BraveP3AEnabled" = false;
             "BravePlaylistEnabled" = false;
+            "DnsOverHttpsMode" = "secure";
+            "MetricsReportingEnabled" = false;
+            "PasswordManagerEnabled" = false;
+            "SafeBrowsingExtendedReportingEnabled" = false;
         };
         extensions = [
           "ghmbeldphafepmbegfdlkpapadhbakde" # Proton Pass
